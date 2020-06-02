8e7dc85f328a33048dcba04b5733472a5b0b2715
jonhlop <jonathan.lopher@hotmail.com>
2020-06-02 15:31:44 +0200

    no message

--- ProyectosExpress/appGimnasios/app.js
+++ ProyectosExpress/appGimnasios/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const clientesRouter = require('./routes/clientes')
+
 
 var app = express();
 
@@ -15,20 +15,28 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req, res) => {
+  if (req.url == '/') {
+    res.redirect('/clientes')
+  }
+
+})
+
+app.use('/clientes', clientesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
